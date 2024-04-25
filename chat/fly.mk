696a462afcd51c4e02a70c33b13eaed51d18e12b
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-04-25 11:36:48 -0400

    make:deploy:ips-list

--- chat/fly.mk
+++ chat/fly.mk
@@ -1,4 +1,6 @@
-flyctl-ips-list:### 	flyctl-ips-list
-	flyctl ips list
-flyctl-deploy--remote-only:### 	flyctl-deploy--remote-only
+deploy:flyctl-deploy--remote-only### 	deploy
+flyctl-deploy--remote-only:
 	flyctl deploy --remote-only
+ips-list:flyctl-ips-list### 	ips-list
+flyctl-ips-list:
+	flyctl ips list
